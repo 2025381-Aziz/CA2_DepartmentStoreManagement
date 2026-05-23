57c99dd9496e2e029aa5e80b7d56fb4c0ef3d798
2025381-Aziz <2025381@student.cct.ie>
2026-05-23 21:04:10 +0100

    Created menu option enums

---
+++ MenuOption.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CA_2.enums;
+
+public enum MenuOption {
+
+    SORT,
+    SEARCH,
+    ADD_EMPLOYEE,
+    CREATE_BINARY_TREE,
+    DISPLAY_TREE,
+    EXIT
+
+}
